a3d25267396ba148d52668ea6d047aded7e56699
sakai1250 <200442066@ccalumni.meijo-u.ac.jp>
2025-09-02 15:55:02 +0900

    remove debug banner and fix policy

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: '超kAI蔵',
       theme: ThemeData(
         useMaterial3: true,
